f3474c91f54152541e9b9602f8015089515ff7f4
Christiaan de Die le Clercq <christiaan@droiddev.nl>
2014-05-04 22:26:58 +0200

    #88 things

--- wp-content/themes/pirateparty/partials/content-single.php
+++ wp-content/themes/pirateparty/partials/content-single.php
@@ -21,7 +21,7 @@ tha_entry_before(); ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content clearfix">
-		<p style="float: left;"> <?php echo get_the_post_thumbnail(the_ID(), 'medium', 'style="margin-right: 7px;"'); ?> </p>  
+		<p style="float: left;"> <?php echo get_the_post_thumbnail('', 'medium', 'style="margin-right: 7px;"'); ?> </p>  
 		<?php
 		the_content();
 		the_bootstrap_link_pages(); ?>
