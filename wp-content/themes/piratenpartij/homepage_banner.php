4874fe56aaece48759afc97554376284d5936427
Patrick Godschalk <argure@argure.nl>
2014-07-14 00:30:40 +0200

    like seriously...

--- wp-content/themes/piratenpartij/homepage_banner.php
+++ wp-content/themes/piratenpartij/homepage_banner.php
@@ -1,47 +1,15 @@
-<?php /*
-
-THE TOOL KIT
-
-#MODAL
-
-<div class="modal hide fade" tabindex="-1" id="watch-pres" style="width: auto; margin-left: -360px;">
-	<div class="modal-header">
-		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">
-			&times;
-		</button>
-		<h3>Video</h3>
-	</div>
-	<div class="modal-body" style="max-height: none;">
-		<embed id="playerid" class="youtube" style="width:760px; height: 360px;" allowfullscreen="true" allowscriptaccess="always" quality="high" bgcolor="#000000" name="playerid" style="" src="http://www.youtube.com/v/MVbeoSPqRs4?enablejsapi=1&version=3&playerapiid=ytplayer" type="application/x-shockwave-flash">
-	</div>
-	<div class="modal-footer">
-		<a href="#" class="btn" data-dismiss="modal">Close</a>
-	</div>
-</div>
-
-# SECRET / SPYING / SURVEILLANCE BLOCK
-
-<div class="span4 t3">
-<p style="margin-top: 15px;"><strong>The Government wants<br>to spy on you 24/7</strong></p>
-<p>Sign our Senate petition against data retention</p>
-<p class="read-more"><a href="<?php echo get_permalink(get_page_by_path('/natsecinquiry-petition/')) ?>" class="btn btn-primary">sign now</a></p>
-</div>
-
-
-*/
-
-?>
-
 <div id="banner">
-
 	<div class="row-fluid lower">
-	
-		<div class="span4 centre" style="background-color: #643794;">
-			<p class="first centre" style="margin-top: 20px;font-size: 26px;line-height:26px;"><strong>Doneer aan de Piratenpartij</strong></p>
-			<p style="margin-top: 10px;"><a class="btn btn-success sidebar-action-item pig" href="<?php echo donate_page_uri() ?>" title="Doneren"><i class="is2-donate"></i>Doneren</a></p>
+
+		<div class="span4 t2" style="background-color: #643794;">
+			<p class="first centre" style="margin-top: 25px;"><strong>Lorem ipsum</strong></p>
+			<p>dolor sit amet</p>
+			<p class="read-more">
+				<a href="https://opinie.piratenpartij.nl" class="btn btn-primary" title="Opinie">knopje!</a>
+			</p>
 		</div>
-		
-		<div class="span4" style="background-color: #643794;">
+
+		<div class="span4 t4 centre" style="background-color: #643794;">
 			<p class="first centre" style="margin-top: 15px;font-size: 22px; line-height: 26px;"><strong>Waarom Piraten?</strong></p>
 			<p class="first centre" style="margin-top: 15px;font-size: 18px; line-height: 22px;">Onze standpunten</p>
 			<p class="read-more centre" style="margin-top: 10px;">
@@ -50,7 +18,7 @@ THE TOOL KIT
 				</a>
 			</p>
 		</div>
-		
+
 		<div class="span4 t1 last">
 			<p style="margin-top: 25px;"><strong>Ontvang onze nieuwsbrief</strong></p>
 			<p>direct in je inbox!</p>
@@ -58,6 +26,6 @@ THE TOOL KIT
 				<a href="nieuwsbrief/" class="btn btn-primary" title="Ontvang onze nieuwsbrief">abonneren</a>
 			</p>
 		</div>
-		
+
 	</div>
 </div>
