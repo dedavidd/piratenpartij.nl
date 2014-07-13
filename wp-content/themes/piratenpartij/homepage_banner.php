ef137dac65d85008aa8f7cc7b12e5b3236c17fd1
Patrick Godschalk <argure@argure.nl>
2014-07-14 00:41:45 +0200

    ok

--- wp-content/themes/piratenpartij/homepage_banner.php
+++ wp-content/themes/piratenpartij/homepage_banner.php
@@ -2,20 +2,18 @@
 	<div class="row-fluid lower">
 
 		<div class="span4 t2">
-			<p class="first centre" style="margin-top: 25px;"><strong>Lorem ipsum</strong></p>
-			<p>dolor sit amet</p>
+			<p class="first centre" style="margin-top: 25px;"><strong>Bekijk ook</strong></p>
+			<p>onze opiniestukken</p>
 			<p class="read-more">
-				<a href="https://opinie.piratenpartij.nl" class="btn btn-primary" title="Opinie">knopje!</a>
+				<a href="https://opinie.piratenpartij.nl" class="btn btn-primary" title="Opinie">lees meer</a>
 			</p>
 		</div>
 
 		<div class="span4 t4 centre" style="background-color: #643794;">
-			<p class="first centre" style="margin-top: 15px;font-size: 22px; line-height: 26px;"><strong>Waarom Piraten?</strong></p>
-			<p class="first centre" style="margin-top: 15px;font-size: 18px; line-height: 22px;">Onze standpunten</p>
-			<p class="read-more centre" style="margin-top: 10px;">
-				<a href="/standpunten/" class="btn btn-primary" title="Onze standpunten">
-					lees meer
-				</a>
+			<p class="first centre" style="margin-top: 25px;"><strong>Waarom Piraten?</strong></p>
+			<p>Onze standpunten</p>
+			<p class="read-more centre">
+				<a href="/standpunten/" class="btn btn-primary" title="Onze standpunten">lees meer</a>
 			</p>
 		</div>
 
@@ -23,7 +21,7 @@
 			<p style="margin-top: 25px;"><strong>Ontvang onze nieuwsbrief</strong></p>
 			<p>direct in je inbox!</p>
 			<p class="read-more">
-				<a href="nieuwsbrief/" class="btn btn-primary" title="Ontvang onze nieuwsbrief">abonneren</a>
+				<a href="/nieuwsbrief/" class="btn btn-primary" title="Ontvang onze nieuwsbrief">abonneren</a>
 			</p>
 		</div>
 
