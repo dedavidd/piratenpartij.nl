df95070d83c130af1687bd98f1178a38dd346a0a
Patrick Godschalk <argure@argure.nl>
2014-07-13 22:28:32 +0200

    BEGONE

--- wp-content/plugins/woocommerce/templates/single-product/related.php
+++ wp-content/plugins/woocommerce/templates/single-product/related.php
@@ -31,7 +31,7 @@ $woocommerce_loop['columns'] = $columns;
 
 if ( $products->have_posts() ) : ?>
 
-	<div class="related products">
+	<!---<div class="related products">
 
 		<h2><?php _e( 'Related Products', 'woocommerce' ); ?></h2>
 
@@ -45,7 +45,7 @@ if ( $products->have_posts() ) : ?>
 
 		<?php woocommerce_product_loop_end(); ?>
 
-	</div>
+	</div>-->
 
 <?php endif;
 
